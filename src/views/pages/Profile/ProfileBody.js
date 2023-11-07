842cb0415a0124d67b026a50ac73f88855c454ac
Quinne2002 <122085456+Quinne2002@users.noreply.github.com>
2023-11-07 15:09:44 +0700

    update

--- src/views/pages/Profile/ProfileBody.js
+++ src/views/pages/Profile/ProfileBody.js
@@ -1,6 +1,24 @@
 import React, { useState } from "react";
 import { AlignJustify, Rss, Info } from "react-feather";
-import { Card, CardImg, CardBody, Collapse, Navbar, Nav, NavItem, NavLink, Button, Row, Col, CardTitle, CardSubtitle } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardBody,
+  Collapse,
+  Navbar,
+  Nav,
+  NavItem,
+  NavLink,
+  Button,
+  Row,
+  Col,
+  CardTitle,
+  CardSubtitle,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "reactstrap";
 import Information from "./Information";
 import Course from "./Course";
 import { Box } from "@mui/material";
@@ -20,8 +38,18 @@ const ProfileBody = () => {
 
   const courses = [
     { name: "Course 1", created: "2023-01-15", students: 50, status: "recent" },
-    { name: "Course 2", created: "2022-10-20", students: 30, status: "in-progress" },
-    { name: "Course 3", created: "2022-08-05", students: 70, status: "completed" },
+    {
+      name: "Course 2",
+      created: "2022-10-20",
+      students: 30,
+      status: "in-progress",
+    },
+    {
+      name: "Course 3",
+      created: "2022-08-05",
+      students: 70,
+      status: "completed",
+    },
   ];
 
   const handleTabChange = (tab) => {
@@ -31,6 +59,14 @@ const ProfileBody = () => {
   const toggle = () => {
     setIsOpen(!isOpen);
   };
+
+//   const card = [
+//     {name: "flash-card",  src: "https://quizlet.com/static/achievements/badge-StudiedWithFlashcards.svg", alt: "Flash Card"},
+//     {name: "learn",  src: "https://quizlet.com/static/achievements/badge-StudiedWithLearn.svg", alt: "Learn"},
+//     {name: "night-owl",  src: "https://quizlet.com/static/achievements/badge-NightOwl.svg", alt: "Night Owl"},
+//     {name: "early bird",  src: "https://quizlet.com/static/achievements/badge-EarlyBird.svg", alt: "Early Bird"},
+//     {name: "test",  src: "https://quizlet.com/static/achievements/badge-StudiedWithTest.svg", alt: "Test"},
+// ]
 
   return (
     <Box>
@@ -80,40 +116,73 @@ const ProfileBody = () => {
         </CardBody>
       </Card>
 
-      <h4>Studying</h4>
+      <h4 className="mt-4 mb-2"><b>Studying</b></h4>
       <Card>
         <CardBody>
           <Row>
-            <Col>
-            <img src="https://quizlet.com/static/achievements/badge-StudiedWithFlashcards.svg" alt ="Flash Card"/>
-            <CardTitle>Flash Card</CardTitle>
-            <CardSubtitle>23 tháng 4, 2023</CardSubtitle>
+            <Col className="text-center">
+              <img
+                src="https://quizlet.com/static/achievements/badge-StudiedWithFlashcards.svg"
+                alt="Flash Card"
+                onClick={toggle}
+                style={{ cursor: 'pointer' }}
+              />
+              <CardTitle>Flash Card</CardTitle>
+              <CardSubtitle>23 tháng 4, 2023</CardSubtitle>
             </Col>
-            <Col>
-            <img src="https://quizlet.com/static/achievements/badge-StudiedWithLearn.svg" alt ="Learn"/>
-            <CardTitle>Learn</CardTitle>
-            <CardSubtitle>23 tháng 4, 2023</CardSubtitle>
+            <Col className="text-center">
+              <img
+                src="https://quizlet.com/static/achievements/badge-StudiedWithLearn.svg"
+                alt="Learn"
+                onClick={toggle}
+                style={{ cursor: 'pointer' }}
+              />
+              <CardTitle>Learn</CardTitle>
+              <CardSubtitle>23 tháng 4, 2023</CardSubtitle>
             </Col>
-            <Col>
-            <img src="https://quizlet.com/static/achievements/badge-NightOwl.svg" alt ="NightOwl"/>
-            <CardTitle>Night Owl</CardTitle>
-            <CardSubtitle>23 tháng 4, 2023</CardSubtitle>
+            <Col className="text-center">
+              <img
+                src="https://quizlet.com/static/achievements/badge-NightOwl.svg"
+                alt="NightOwl"
+                onClick={toggle}
+                style={{ cursor: 'pointer' }}
+              />
+              <CardTitle>Night Owl</CardTitle>
+              <CardSubtitle>23 tháng 4, 2023</CardSubtitle>
             </Col>
-            <Col>
-            <img src="https://quizlet.com/static/achievements/badge-EarlyBird.svg" alt ="EarlyBird"/>
-            <CardTitle>Early Bird</CardTitle>
-            <CardSubtitle>23 tháng 4, 2023</CardSubtitle>
+            <Col className="text-center">
+              <img
+                src="https://quizlet.com/static/achievements/badge-EarlyBird.svg"
+                alt="EarlyBird"
+                onClick={toggle}
+                style={{ cursor: 'pointer' }}
+              />
+              <CardTitle>Early Bird</CardTitle>
+              <CardSubtitle>23 tháng 4, 2023</CardSubtitle>
             </Col>
-            <Col>
-            <img src="https://quizlet.com/static/achievements/badge-StudiedWithTest.svg" alt ="Test"/>
-            <CardTitle>Test</CardTitle>
-            <CardSubtitle>23 tháng 4, 2023</CardSubtitle>
+            <Col className="text-center">
+              <img
+                src="https://quizlet.com/static/achievements/badge-StudiedWithTest.svg"
+                alt="Test"
+                onClick={toggle}
+                style={{ cursor: 'pointer' }}
+              />
+              <CardTitle>Test</CardTitle>
+              <CardSubtitle>23 tháng 4, 2023</CardSubtitle>
             </Col>
           </Row>
         </CardBody>
       </Card>
+      <Modal isOpen={isOpen} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Thông báo</ModalHeader>
+        <ModalBody>
+          Bạn hãy cố gắng hoàn thành hết các thẻ học trong quizz nhé để nhận được huy hiệu
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
     </Box>
-      
   );
 };
 
