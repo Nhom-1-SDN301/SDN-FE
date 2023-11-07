e05bda3796a1ce80ad40f02caa4d060844a2cb28
shiho2305 <qlinh2305@gmail.com>
2023-11-08 02:51:21 +0700

    update streak for profile

--- src/views/pages/Profile/ProfileBody.js
+++ src/views/pages/Profile/ProfileBody.js
@@ -10,6 +10,10 @@ import {
   NavItem,
   NavLink,
   Button,
+  CardTitle,
+  CardSubtitle,
+  Col, 
+  Row
 } from "reactstrap";
 import Information from "./Information";
 import Course from "./Course";
@@ -43,6 +47,23 @@ const ProfileBody = () => {
       status: "completed",
     },
   ];
+
+  const Streaks = [
+    {name: "3-day streak", image:"https://quizlet.com/static/achievements/badge-Day.svg", time:"Earned 01/08/22", number:"3" }, 
+    {name: "5-day streak", image:"https://quizlet.com/static/achievements/badge-Day.svg", time:"Earned 19/08/22", number:"5" }, 
+    {name: "7-day streak", image:"https://quizlet.com/static/achievements/badge-Day.svg", time:"Earned 18/09/22", number:"7" }, 
+    {name: "10-day streak", image:"https://quizlet.com/static/achievements/locked-badge-Day.svg", time:"", number:"10" }, 
+    {name: "20-day streak", image:"https://quizlet.com/static/achievements/locked-badge-Day.svg", time:"", number:"20" }, 
+    {name: "30-day streak", image:"https://quizlet.com/static/achievements/locked-badge-Day.svg", time:"", number:"30" }, 
+    {name: "45-day streak", image:"https://quizlet.com/static/achievements/locked-badge-Day.svg", time:"", number:"45" }, 
+    {name: "60-day streak", image:"https://quizlet.com/static/achievements/locked-badge-Day.svg", time:"", number:"60" }, 
+    {name: "70-day streak", image:"https://quizlet.com/static/achievements/locked-badge-Day.svg", time:"", number:"70" }, 
+    {name: "80-day streak", image:"https://quizlet.com/static/achievements/locked-badge-Day.svg", time:"", number:"80" }
+  ];
+
+  const [showAll, setShowAll] = useState(false);
+  const displayedStreaks = showAll ? Streaks : Streaks.slice(0, 4);
+
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
@@ -95,6 +116,52 @@ const ProfileBody = () => {
       </Card>
       {activeTab === "info" && <Information personalInfo={personalInfo} />}
       {activeTab === "course" && <Course courses={courses} />}
+
+      <h4 className="mt-4 mb-2 fw-bolder">Streaks</h4>
+      <Card className="p-4">
+        <CardBody>
+          <h4 className="mb-2">Daily Streaks</h4>
+          <Row>
+              {
+                displayedStreaks.map((streak, index) => (
+                  <Col className="text-center mt-3" lg="3" md="6" key={index}>
+                    <div style={{ position: "relative" }}>
+                      <img src={streak?.image} alt={streak?.name} />
+                      <span
+                        style={{
+                          position: "absolute",
+                          top: "50%",
+                          left: "50%",
+                          transform: "translate(-50%, -50%)",
+                          color:"black", 
+                          fontWeight:"bolder", 
+                          fontSize:"large"
+                        }}
+                      >
+                        {streak?.number}
+                      </span>
+                    </div>
+                    <CardTitle>{streak?.name}</CardTitle>
+                    <CardSubtitle>{streak?.time}</CardSubtitle>
+                  </Col>
+                ))
+              }
+          </Row>
+          <Row className="text-center mt-3">
+            <div className="d-flex justify-content-center">
+              <Button
+                color="info"
+                style={{ width: "120px" }}
+                onClick={() => setShowAll(!showAll)}
+              >
+                {showAll ? "View less" : "View all"}
+              </Button>
+            </div>
+          </Row>
+        </CardBody>
+      </Card>
+
+
     </Box>
   );
 };
